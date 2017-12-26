a78b87ae47f80ae750f851e3e331a73f39ff2168
Alejandro Guerra <tylez87@gmail.com>
2017-12-26 12:09:28 +0100

    error

--- AccumulateExample.swift
+++ AccumulateExample.swift
@@ -1,7 +1,7 @@
 extension Array {
     func accumulate<S>(_ yield: (Element) -> S) -> [S] {
         var result: [S] = [S]()
-        for item in self {
+        for item in self
             result.append(yield(item))
         }
         return result
